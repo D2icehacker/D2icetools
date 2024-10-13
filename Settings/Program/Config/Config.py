22db06b2f13900f1839dde661859341a8dcefdb2
Daniel Godwin <danielgodwin00j@gmail.com>
2024-10-13 20:33:17 +0100

    update

---
+++ Settings/Program/Config/Config.py
@@ -0,0 +1,25 @@
+# Copyright (c) RedTiger (https://redtiger.shop)
+# See the file 'LICENSE' for copying permission
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------|
+# EN: 
+#     - Do not touch or modify the code below. If there is an error, please contact the owner, but under no circumstances should you touch the code.
+#     - Do not resell this tool, do not credit it to yours.
+# FR: 
+#     - Ne pas toucher ni modifier le code ci-dessous. En cas d'erreur, veuillez contacter le propriétaire, mais en aucun cas vous ne devez toucher au code.
+#     - Ne revendez pas ce tool, ne le créditez pas au vôtre.
+
+name_tool = "D2iceTools"
+type_tool = "Multi-Tools"
+version_tool = "1.0"
+coding_tool = "Python 3"
+language_tool = "EN"
+creator = "D2icehacker"
+platform = "Windows 10/11 & Linux"
+discord_server = "discord.gg/heisd2ice"
+website = "github.com/d2icehacker"
+github_tool = "github.com/d2icehacker"
+telegram = "t.me/d2icee"
+url_config = ""
+url_downloads = ""
+license = ""
+copyright = "Copyright (c) D2iceTools 'LICENSE'"
